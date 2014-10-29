14e8ab05ca9a4f78456dcec031a2f48207312782
DNRKlein <David.Klein1994@gmail.com>
2014-10-29 22:12:39 +0100

    Added TrueFalse class to model the questions

---
+++ app/src/main/java/com/github/dnrklein/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.github.dnrklein.geoquiz;
+
+public class TrueFalse {
+    //mQuestion is int since it will hold resource ID of string resources for question
+    //mTrueQuestion is whether question is false or true
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    //generated getters and setters
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
