1a257d8f7af5813653e46342fb731b660bc5823e
DNRKlein <David.Klein1994@gmail.com>
2015-03-04 20:45:46 +0100

    Removed the logtexts

--- app/src/main/java/com/github/dnrklein/geoquiz/QuizActivity.java
+++ app/src/main/java/com/github/dnrklein/geoquiz/QuizActivity.java
@@ -18,9 +18,6 @@ public class QuizActivity extends Activity {
     private Button mFalseButton;
     private Button mNextButton;
 
-    //constant for debugging
-    private static final String TAG = "QuizActivity";
-
     //the textview
     private TextView mQuestionTextView;
 
@@ -66,7 +63,6 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         //Setting question in textview using getQuestion() from the TrueFalse class
@@ -114,31 +110,26 @@ public class QuizActivity extends Activity {
     @Override
     public void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart() called");
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause() called");
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume() called");
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop() called");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
